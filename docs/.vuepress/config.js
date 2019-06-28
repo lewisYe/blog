fe242dd6a9b4c222989dd8f1fa18ac9c35c4b325
lewisYe <supermasterye@163.com>
2019-06-28 11:25:46 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  title: 'Hello VuePress',
+  description: 'Just playing around',
+  themeConfig: {
+    nav: [
+      { text: 'Github', link: 'https://github.com/lewisYe' },
+    ],
+    sidebar: [
+      '/',
+    ],
+    sidebarDepth: 2
+  }
+}
